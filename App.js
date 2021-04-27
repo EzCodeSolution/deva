a648aed7e4fac0dc27731fda6702afb915c4819a
thanaha <thanaha@thanahas-MacBook-Pro.local>
2021-04-27 09:46:39 +0700

    try1

--- App.js
+++ App.js
@@ -5,7 +5,8 @@ const bodyParser = require('body-parser')
 
 const path=require("path")
 
-app.use(express.static(__dirname + '../public'));
+app.use(express.static(path.join('app/public')));  //forheroku
+// app.use(express.static(path.join('app/public'))); //fordev
 
 app.set('views',path.join(__dirname, '/public/'))
 app.set('view engine', 'ejs')

340d85d9439cc759cf37a28981aafc35cfe610be
thanaha <thanaha@thanahas-MacBook-Pro.local>
2021-04-27 09:34:16 +0700

    afd

--- App.js
+++ App.js
@@ -4,7 +4,8 @@ const homeroute = require("./routes/web/home.route");
 const path=require("path")
 
 app.use(express.static("public"))
-app.set('views',path.join(__dirname, '/public/'))
+
+app.set('views',path.join(__dirname, '/public'))
 app.set('view engine', 'ejs')
 
 app.use(homeroute);
